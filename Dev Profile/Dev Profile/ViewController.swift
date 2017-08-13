3ca10ffd3a405eef1e53a447baedb83f8a81be4e
Clement M <massardc@users.noreply.github.com>
2017-08-14 00:17:21 +0200

    Developed main screen for iPhone

--- Dev Profile/Dev Profile/ViewController.swift
+++ Dev Profile/Dev Profile/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileLogo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        profileLogo.clipsToBounds = true
+        profileLogo.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
